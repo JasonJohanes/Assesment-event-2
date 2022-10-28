0bec6b96c884fd7d1a172e34ab9e0324f839e433
JasonJohanes <jason.johanes.stu@elyon.sch.id>
2022-10-28 13:15:20 +0700

    fuck

--- Assesment 2 artist list.js
+++ Assesment 2 artist list.js
@@ -2,23 +2,34 @@ const sqlite3 = require('sqlite3').verbose();
 
 let db = new sqlite3.Database ('Artist_list.db');
 
-let sql = `SELECT *
-           FROM Artist
+//sorting system//
+let sql = `SELECT * FROM Artist
            JOIN Song ON Artist.Artist_ID = Song.Artist_ID
-           ORDER BY Name`;
-
+           ORDER BY Streams DESC`;
 
 db.all(sql, (err, rows)=> {
     console.log(err)
-    console.log(rows);
+    console.log(rows)
 });
+//sorting system//
 
 
-let del = `DELETE FROM Artist WHERE Artist_ID = A21`;
+//delete//
+db.each ('DELETE FROM Song WHERE Song.artist_id NOT IN ( SELECT Song.artist_ID FROM Song INNER JOIN Artist ON Song.Artist_id = Artist.artist_id);', (err, row) => {
+    console.log(row);
+    })
 
-db.run(del, (err, rows)=> {
-    console.log(err)
-    console.log(rows);
+db.close((err) => {
+if (err){
+    return console.error(err.message);
+}
+console.log('closing nodelogin database.');
 });
+//delete//
 
+
+//Insert//
+let masuk = `INSERT INTO Artist (Artist_ID, Artist_Name)
+             VALUES ('A23' , 'MARSHMELLO')`;
+//Insert//
 db.close();
